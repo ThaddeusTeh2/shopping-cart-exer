b2b0973ad8dc4eeccadbc194925387b23bcc1ce4
Thaddeus Teh :D <thaddeusteh2@gmail.com>
2024-11-13 11:27:07 +0800

    filter-functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import ItemList from "./components/list";
-import Item from "./components/item";
 
 // Test data - Do not modify
 const items = [
@@ -11,11 +9,86 @@ const items = [
   { id: 5, name: "Shoes", price: 65, onSale: true },
 ];
 
+//functions
+
+//item
+function Item(props) {
+  //set default value
+  const { name = "N/A", price = 0, onSale = false } = props;
+  //item xml
+  return (
+    <div className="col-lg-4">
+      <div className="card m-1" style={{ width: "18rem," }}>
+        <div className="card-body">
+          <h5 className="card-title">
+            {onSale ? (
+              <div class="bg-success rounded text-center">On sale</div>
+            ) : (
+              <div class="text-center">Not on sale</div>
+            )}
+          </h5>
+          <h6 className="card-subtitle mb-2"> {name}</h6>
+          <p className="card-text">${price}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+//list
+function ItemList(props) {
+  const { list } = props;
+  return (
+    <div className="row">
+      {list.map((item) => (
+        <Item key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+
+//app
 function App() {
-  // Your code here
+  //keeps track if showing sale items
+  const [showSaleItems, setShowSaleItems] = useState(false);
+  //if true, show sale items, if false, show items
+  const filteredItems = showSaleItems
+    ? items.filter((item) => item.onSale)
+    : items;
+
+  //toggle button
+  const toggleSaleItems = () => {
+    setShowSaleItems((prevState) => !prevState);
+  };
+
+  /*
+  prevState is a callback function parameter, represents current value of showSaleItems state.
+
+  • function toggleSaleItems toggles value of showSaleItems between true and false.
+	• arrow function prevState => !prevState ensures that new state is always opposite of current state, enabling toggling behavior.
+  
+  •	!prevState used to toggle state:
+  •	If prevState is true, !prevState evaluates to false.
+  •	If prevState is false, !prevState evaluates to true.
+  */
+
   return (
     <div className="container">
-      <ItemList list={items} />
+      <div className="card m-5">
+        <div className="m-1">
+          <button
+            onClick={toggleSaleItems}
+            className="btn btn-outline-dark mb-3"
+          >
+            {showSaleItems ? "Show All Items" : "Show Sale Items Only"}
+          </button>
+        </div>
+        <div className="row">
+          {filteredItems.map((item) => (
+            <Item key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
